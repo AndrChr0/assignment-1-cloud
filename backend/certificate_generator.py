58206f3996841ba53be2a7721d18e16cd011c3f8
Andreas Christiansen <andycchhrr@gmail.com>
2024-03-20 17:27:08 +0100

    poort

--- backend/certificate_generator.py
+++ backend/certificate_generator.py
@@ -53,8 +53,9 @@ def download_file():
 
 
 if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 for local development
     print("This should not appear when run with Gunicorn")
+    print(port)
     print(app.secret_key)
     # app.run(debug=True)
-    port = int(os.environ.get('PORT', 5000))  # Default to 5000 for local development
     # app.run(host='0.0.0.0', port=port)
