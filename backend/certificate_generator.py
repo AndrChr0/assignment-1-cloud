b8278db1d16bcb89867361b28a68586367004825
Andreas Christiansen <andycchhrr@gmail.com>
2024-03-20 17:41:07 +0100

    minor chANGE

--- backend/certificate_generator.py
+++ backend/certificate_generator.py
@@ -53,6 +53,7 @@ def download_file():
 
 
 if __name__ == '__main__':
+    app.run(debug=True)
     port = int(os.environ.get('PORT', 5000))  # Default to 5000 for local development
     print("This should not appear when run with Gunicorn")
     print(port)
